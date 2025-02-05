79bdf0aa957c3c2f695b67f8269edfb0b5cddcd5
turinglabs <sebastiano.cataudo@gmail.com>
2025-02-05 09:29:38 +0100

    adding transfer ownership and proxy

--- scripts/factory/create.js
+++ scripts/factory/create.js
@@ -18,13 +18,15 @@ async function main() {
     const provider = new ethers.providers.JsonRpcProvider(configs.provider);
     let wallet = new ethers.Wallet(configs.owner_key).connect(provider);
     const contract = new ethers.Contract(
-        configs.contract_address,
+        configs.factory_address,
         ABI.abi,
         wallet
     );
 
     const name = "My Awesome Event";
     const ticker = "MYEVENT";
+    const proxyAddress = "0x42694cac013b230e035f85bc2e158aff49bfe4cf";
+
     try {
         const gasPrice = (await provider.getGasPrice()).mul(2);
         const resultPub = await contract.createMegoTicketsPublic(name, ticker, {
@@ -35,6 +37,22 @@ async function main() {
         const deployedEvent = receipt.events.find(event => event.event === "MegoTicketsPublicDeployed");
         if (deployedEvent) {
             console.log("Contract created at:", deployedEvent.args.deployedAddress);
+            const MEGO_ABI = JSON.parse(
+                fs
+                    .readFileSync(
+                        "./artifacts/contracts/MegoTicketsPublic.sol/MegoTicketsPublic.json"
+                    )
+                    .toString()
+            );
+            const megoContract = new ethers.Contract(deployedEvent.args.deployedAddress, MEGO_ABI.abi, wallet);
+            const ownerOfContract = await megoContract.owner();
+            console.log("Owner of contract:", ownerOfContract);
+            const result = await megoContract.setProxyAddress(proxyAddress, true, {
+                gasPrice,
+            });
+            console.log("Waiting at: " + result.hash);
+            await result.wait();
+            console.log("Proxy address set correctly");
         } else {
             console.log("Deployed event not found in the transaction receipt.");
         }
